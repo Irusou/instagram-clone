2a1e5d82c946bba2d89aba7f1e625e1c2f9b90d2
João Pedro <irus0ubusiness@gmail.com>
2023-11-01 03:24:18 +0000

    Revert "Revert "feat: images finally load + first attempt at making a carousel""
    This reverts commit 85fbbaafcd403ef7c139d13213d5d678c44ed315.

--- frontend/src/components/ProfilePhoto/ProfilePhoto.tsx
+++ frontend/src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -1,14 +1,10 @@
 import "./ProfilePhoto.css";
+import logo from "C:/Users/JoaoPedro/Desktop/dev/instagram-clone/frontend/public/eu.jpg";
 
 export const ProfilePhoto = () => {
-	const url = "/frontend/public/eu.jpg";
-
 	return (
 		<>
-			{/* 
-        TODO: solve image not loading error
-      */}
-			<img src={url} />
+			<img src={logo} alt="logo" />
 		</>
 	);
 };
